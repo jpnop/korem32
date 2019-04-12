e0606a9929e575af033c7e50a8a9effc6c174b65
Hansungjoo <21700768@handong.edu>
2019-04-12 12:49:33 +0900

    hello

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-void sub1();{}
+void sub1();
+void sub4(){
+ for(int i=0; i<20; i++){
+        printf("HyunSukKang Babo~");
+}
+
+
 
 int main(){
 
 sub1();
-
+sub4();
 return 0;
 }
